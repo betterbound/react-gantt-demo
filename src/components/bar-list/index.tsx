cd758c259ccc014e9ec2ff8965bfe8099a37a29d
yukie.terasaki <yukie_terasaki@betterbound.jp>
2023-06-13 17:17:06 +0900

    Create the component to display multipul items on each task bar

--- src/components/bar-list/index.tsx
+++ src/components/bar-list/index.tsx
@@ -5,19 +5,26 @@ import Context from '../../context'
 import GroupBar from '../group-bar'
 import InvalidTaskBar from '../invalid-task-bar'
 import TaskBar from '../task-bar'
+import TaskBarItems from '../task-bar-items'
 
 const BarList: React.FC = () => {
   const { store } = useContext(Context)
   const barList = store.getBarList
   const { count, start } = store.getVisibleRows
   return (
-    <>
-      {barList.slice(start, start + count).map(bar => {
+    <div data-bar='BarList'>
+      {barList.slice(start, start + count).map((bar, index) => {
         if (bar._group) return <GroupBar key={bar.key} data={bar} />
+
+        if (bar.record.barItems) {
+          return bar.record.barItems.map(barItem => (
+            <TaskBarItems key={barItem.id} index={index} data={bar} barItem={barItem} />
+          ))
+        }
 
         return bar.invalidDateRange ? <InvalidTaskBar key={bar.key} data={bar} /> : <TaskBar key={bar.key} data={bar} />
       })}
-    </>
+    </div>
   )
 }
 export default observer(BarList)
