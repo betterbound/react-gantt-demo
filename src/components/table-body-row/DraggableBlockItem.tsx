00ff7eb4e74a05761671a7a97868dc7aec34cdcf
yukie.terasaki <yukie_terasaki@betterbound.jp>
2024-09-03 11:09:59 +0900

    modify: Modify DraggableBlockItem component

--- src/components/table-body-row/DraggableBlockItem.tsx
+++ src/components/table-body-row/DraggableBlockItem.tsx
@@ -1,25 +1,26 @@
-import React, { useContext } from 'react'
-import type {
-  DraggableSyntheticListeners } from '@dnd-kit/core'
-import { observer } from 'mobx-react-lite'
+import type { DraggableSyntheticListeners } from '@dnd-kit/core'
 import classNames from 'classnames'
+import { observer } from 'mobx-react-lite'
+import React, { useContext } from 'react'
 import Context from '../../context'
+import type GanttStore from '../../store'
 import type { Gantt } from '../../types'
 import ObserverTableRows from '../table-body-rows'
-import type GanttStore from '../../store'
 import RowToggler from './RowToggler'
 
 interface ExpandIconProps {
   bar: Gantt.Bar
   onExpand?: (record: Gantt.Record, collapsed: boolean) => void
   store: GanttStore
-  expandIcon?: (props: { level: number; collapsed: boolean; onClick: (event: React.MouseEvent<HTMLDivElement>) => void }) => React.ReactNode
+  expandIcon?: (props: {
+    level: number
+    collapsed: boolean
+    onClick: (event: React.MouseEvent<HTMLDivElement>) => void
+  }) => React.ReactNode
   prefixCls: string
-  tableIndent: number
 }
 interface DraggableBlockItemProps {
   bar: Gantt.Bar
-  isLastChild: boolean
   isActive?: boolean
   listeners?: DraggableSyntheticListeners
   transform?: { x: number; y: number }
@@ -27,24 +28,15 @@ interface DraggableBlockItemProps {
   setActivatorNodeRef?: (element: HTMLElement | null) => void
 }
 
-const ExpandIcon = observer(({ bar, onExpand, store, expandIcon, prefixCls, tableIndent }: ExpandIconProps) => {
-  const handleClick = (event) => {
+const ExpandIcon = observer(({ bar, onExpand, store, expandIcon, prefixCls }: ExpandIconProps) => {
+  const handleClick = event => {
     event.stopPropagation()
     if (onExpand) onExpand(bar.task.record, !bar._collapsed)
     store.setRowCollapse(bar.task, !bar._collapsed)
   }
 
   return (
-    <div
-      style={{
-        position: 'absolute',
-        left: tableIndent * bar._depth + 15,
-        background: 'white',
-        zIndex: 9,
-        transform: 'translateX(-52%)',
-        padding: 1,
-      }}
-    >
+    <div>
       {expandIcon ? (
         expandIcon({
           level: bar._depth,
@@ -52,19 +44,20 @@ const ExpandIcon = observer(({ bar, onExpand, store, expandIcon, prefixCls, tabl
           onClick: handleClick,
         })
       ) : (
-        <RowToggler
-          prefixCls={prefixCls}
-          level={bar._depth}
-          collapsed={bar._collapsed}
-          onClick={handleClick}
-        />
+        <RowToggler prefixCls={prefixCls} level={bar._depth} collapsed={bar._collapsed} onClick={handleClick} />
       )}
     </div>
   )
 })
 
-
-const DraggableBlockItem = ({ bar, isLastChild, isActive, listeners, transform, transition, setActivatorNodeRef }: DraggableBlockItemProps) => {
+const DraggableBlockItem = ({
+  bar,
+  isActive,
+  listeners,
+  transform,
+  transition,
+  setActivatorNodeRef,
+}: DraggableBlockItemProps) => {
   const { store, onRow, tableIndent, expandIcon, prefixCls, onExpand } = useContext(Context)
   const prefixClsTableBody = `${prefixCls}-table-body`
   const { columns, rowHeight } = store
@@ -77,13 +70,10 @@ const DraggableBlockItem = ({ bar, isLastChild, isActive, listeners, transform, 
     transition,
   }
 
-  if(!bar?.record) return null
+  if (!bar?.record) return null
 
   return (
-    <div
-      ref={setActivatorNodeRef}
-      style={style}
-    >
+    <div ref={setActivatorNodeRef} style={style}>
       <div
         role='none'
         className={classNames(`${prefixClsTableBody}-row`, bar.record.className)}
@@ -91,12 +81,6 @@ const DraggableBlockItem = ({ bar, isLastChild, isActive, listeners, transform, 
           onRow?.onClick(bar.record)
         }}
       >
-        <button type='button' {...listeners}
-          style={{ 
-            cursor: isActive ? 'grabbing' : 'grab',
-            pointerEvents: 'auto',
-            touchAction: 'none'
-          }}> D </button>
         {columns.map((column, index) => (
           <div
             key={column.name}
@@ -111,36 +95,27 @@ const DraggableBlockItem = ({ bar, isLastChild, isActive, listeners, transform, 
               ...column.style,
             }}
           >
-            {index === 0 &&
-          // eslint-disable-next-line unicorn/no-new-array
-          new Array(bar._depth).fill(0).map((_, i) => (
-            <div
-              // eslint-disable-next-line react/no-array-index-key
-              key={i}
-              className={classNames(`${prefixClsTableBody}-row-indentation`, {
-                [`${prefixClsTableBody}-row-indentation-hidden`]: isLastChild && i === bar._depth - 2,
-                [`${prefixClsTableBody}-row-indentation-both`]: i === bar._depth - 1,
-              })}
-              style={{
-                top: -(rowHeight / 2) + 1,
-                left: tableIndent * i + 15,
-                width: tableIndent * 1.5 + 5,
-              }}
-            />
-          ))}
-            {index === 0 && bar._childrenCount > 0 && (
-              <ExpandIcon
-                bar={bar}
-                onExpand={onExpand}
-                store={store}
-                expandIcon={expandIcon}
-                prefixCls={prefixCls}
-                tableIndent={tableIndent}
-              />
+            {column.name === 'dragButton' && (
+              <button
+                type='button'
+                {...listeners}
+                style={{
+                  cursor: isActive ? 'grabbing' : 'grab',
+                  pointerEvents: 'auto',
+                  touchAction: 'none',
+                }}
+              >
+                {column.render ? column.render(bar.record) : 'drag'}
+              </button>
             )}
-            <span className={`${prefixClsTableBody}-ellipsis`}>
-              {column.render ? column.render(bar.record) : bar.record[column.name]}
-            </span>
+            {index === 0 && bar._childrenCount > 0 && (
+              <ExpandIcon bar={bar} onExpand={onExpand} store={store} expandIcon={expandIcon} prefixCls={prefixCls} />
+            )}
+            {column.name !== 'dragButton' && (
+              <span className={`${prefixClsTableBody}-ellipsis`}>
+                {column.render ? column.render(bar.record) : bar.record[column.name]}
+              </span>
+            )}
           </div>
         ))}
       </div>
@@ -148,11 +123,9 @@ const DraggableBlockItem = ({ bar, isLastChild, isActive, listeners, transform, 
         MEMO: ガントチャート側は _collapsed: true の時はデータから削除しているが（デフォルトの仕様）、タイトル側は _collapsed の値で表示・非表示のコントロールしてデータ自体は存在させる。
         Drag & Drop でソートしたデータで上書きする時、_collapsed: true でデータが存在しない状態で上書きすると、childrenのデータがない状態で上書きされてしまうため。
        */}
-      {!bar._collapsed && bar.children && bar.children.length > 0 && (
-        <ObserverTableRows barList={bar.children}/>
-      )}
+      {!bar._collapsed && bar.children && bar.children.length > 0 && <ObserverTableRows barList={bar.children} />}
     </div>
   )
 }
 
-export default React.memo(DraggableBlockItem)
+export default observer(DraggableBlockItem)
